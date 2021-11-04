79ffc8e07c4ca0297892c3f0a33fc823479e4a9b
alexkarunny <smetan4ik1@gmail.com>
2021-11-04 21:03:03 +0300

    Lesson 12, run tests

--- integration/app.test.js
+++ integration/app.test.js
@@ -2,7 +2,7 @@
 describe('addItemForm', () => {
     it('base example, visually looks correct', async () => {
         // APIs from jest-puppeteer
-        await page.goto('http://localhost:9009/iframe.html?id=additemform-component--add-item-form-base-example');
+        await page.goto('http://localhost:9009/iframe.html?id=todolist-additemform--add-item-form-example&globals=backgrounds.grid:false&viewMode=story');
         const image = await page.screenshot();
 
         // API from jest-image-snapshot
